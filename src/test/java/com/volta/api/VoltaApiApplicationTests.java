d6c161f1d30f4b3d692f83822f91f9a29c4bf95c
EnzoHerrera <enzo.dinks@gmail.com>
2026-08-15 12:35:06 -0300

    feat: adiciona estrutura inicial do projeto

---
+++ src/test/java/com/volta/api/VoltaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volta.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoltaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
